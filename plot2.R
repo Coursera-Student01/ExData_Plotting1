4d09d7e9cd0ab5cb64b608510be0bde95ea1bfed
Coursera-Student01 <Coursera.Student01@gmail.com>
2015-10-11 19:56:23 +0300

    Course project upload

---
+++ plot2.R
@@ -0,0 +1,36 @@
+library(base)
+library(lubridate)
+Sys.setlocale("LC_ALL","English")
+
+#Getting data
+
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+              temp)
+table <- read.table(unz(temp, "household_power_consumption.txt"),
+                    header = T, 
+                    sep = ";", 
+                    stringsAsFactors = F,
+                    na.strings="?")
+unlink(temp)
+rm(temp)
+
+#Subsetting & getting data prepared for plotting
+table$Date <- strptime(table$Date, 
+                       format = "%d/%m/%Y")
+
+target_df <- subset(table, Date >= "2007-02-01" & Date <= "2007-02-02")
+
+time_temp_vector <- paste(target_df$Date, target_df$Time)   
+target_df$Time <- strptime(time_temp_vector,
+                           format = "%Y-%m-%d %H:%M:%S")
+rm(time_temp_vector)
+
+#Plotting
+png(file="plot2.png",width=480,height=480)
+par(cex = 0.7)
+plot(target_df$Time, target_df$Global_active_power, 
+     ylab = "Global Active Power (kilowatts)",
+     xlab = "", main = "", type = "l")
+
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,62 @@
+library(base)
+library(lubridate)
+Sys.setlocale("LC_ALL","English")
+
+#Getting data
+
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+              temp)
+table <- read.table(unz(temp, "household_power_consumption.txt"),
+                    header = T, 
+                    sep = ";", 
+                    stringsAsFactors = F,
+                    na.strings="?")
+unlink(temp)
+rm(temp)
+
+#Subsetting & getting data prepared for plotting
+table$Date <- strptime(table$Date, 
+                       format = "%d/%m/%Y")
+
+target_df <- subset(table, Date >= "2007-02-01" & Date <= "2007-02-02")
+
+time_temp_vector <- paste(target_df$Date, target_df$Time)   
+target_df$Time <- strptime(time_temp_vector,
+                           format = "%Y-%m-%d %H:%M:%S")
+rm(time_temp_vector)
+
+#Plotting
+png(file="plot4.png",width=480,height=480)
+par(mfrow = c(2,2))
+#Number 1
+plot(target_df$Time, target_df$Global_active_power, 
+     ylab = "Global Active Power",
+     xlab = "", 
+     main = "", type = "l")
+
+#Number 2
+plot(target_df$Time, target_df$Voltage, 
+     ylab = "Voltage",
+     xlab = "datetime", 
+     main = "", type = "l")
+
+#Number 3
+plot(target_df$Time, target_df$Sub_metering_1, 
+     ylab = "Energy sub metering",
+     xlab = "", main = "", type = "l", col = "black")
+lines(target_df$Time, target_df$Sub_metering_2, col = "red")
+lines(target_df$Time, target_df$Sub_metering_3, col = "blue")
+legend(x = "topright", 
+       legend = c("Sub_metering_1",
+                  "Sub_metering_2",
+                  "Sub_metering_3"),
+       col = c("black", "red", "blue"), bty = "n", lty=1)
+
+#Number 4
+plot(target_df$Time, target_df$Global_reactive_power, 
+     ylab = "Global_reactive_power",
+     xlab = "datetime", 
+     main = "", type = "l")
+
+dev.off()
